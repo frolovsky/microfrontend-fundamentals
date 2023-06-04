7d1fb96c9f0c64d3ba0ab0ce082e41de35d93adb
Алексей Фроловский (Тагес Джамп) <aleksei.frolovskii@tages.ru>
2023-06-04 20:08:19 +0300

    lesson 1 complete

--- 01-vanilla-app/index.js
+++ 01-vanilla-app/index.js
@@ -1,50 +1,52 @@
 import { registerApplication, start } from "single-spa";
 
 // Part 1 - an application that does nothing
-const app = {
-  // Optional
-  async bootstrap(props) {
-    console.log("App is initializing!", props);
-  },
-  // Required
-  async mount(props) {
-    console.log("App is mounting", props);
-  },
-  // Required
-  async unmount(props) {
-    console.log("App is unmounting", props);
-  },
-};
-
-// Part 2 - an application that mounts a button
 // const app = {
-//   // The mount lifecycle function is called every time the application is added to the DOM
+//   // Optional
+//   async bootstrap(props) {
+//     console.log("App is initializing!", props);
+//   },
+//   // Required
 //   async mount(props) {
 //     console.log("App is mounting", props);
-//     // For single-spa applications, the dom element container is selected by the application itself.
-//     // It is conventional to use single-spa-application:app-name as the ID of the container.
-//     // It's highly recommended to use this convention, as single-spa-layout single-spa-react,
-//     // single-spa-vue, single-spa-angular, and other libraries default to using it.
-//     const domElementContainer = document.getElementById(
-//       "single-spa-application:vanilla-app"
-//     );
-
-//     // Since this example does not use a framework, we use browser DOM apis to create the button
-//     const button = document.createElement("button");
-//     button.textContent = `Your ${props.adjective} microfrontend!`;
-//     button.addEventListener("click", () => alert("Button was clicked"));
-
-//     // Each single-spa application should append ("mount") to its dom element container
-//     domElementContainer.appendChild(button);
 //   },
-
-//   // The unmount lifecycle is called every time the application is removed from the DOM
+//   // Required
 //   async unmount(props) {
 //     console.log("App is unmounting", props);
-//     const domElementContainer = document.getElementById("single-spa-application:vanilla-app");
-//     domElementContainer.innerHTML = "";
 //   },
 // };
+
+// Part 2 - an application that mounts a button
+const app = {
+  // The mount lifecycle function is called every time the application is added to the DOM
+  async mount(props) {
+    console.log("App is mounting", props);
+    // For single-spa applications, the dom element container is selected by the application itself.
+    // It is conventional to use single-spa-application:app-name as the ID of the container.
+    // It's highly recommended to use this convention, as single-spa-layout single-spa-react,
+    // single-spa-vue, single-spa-angular, and other libraries default to using it.
+    const domElementContainer = document.getElementById(
+      "single-spa-application:vanilla-app"
+    );
+
+    // Since this example does not use a framework, we use browser DOM apis to create the button
+    const button = document.createElement("button");
+    button.textContent = `Your ${props.adjective} microfrontend!`;
+    button.addEventListener("click", () => alert("Button was clicked"));
+
+    // Each single-spa application should append ("mount") to its dom element container
+    domElementContainer.appendChild(button);
+  },
+
+  // The unmount lifecycle is called every time the application is removed from the DOM
+  async unmount(props) {
+    console.log("App is unmounting", props);
+    const domElementContainer = document.getElementById(
+      "single-spa-application:vanilla-app"
+    );
+    domElementContainer.innerHTML = "";
+  },
+};
 
 // Register your microfrontend with single-spa
 registerApplication({
@@ -58,18 +60,14 @@ registerApplication({
   // app: () => import('./file-with-app.js'),
 
   // The list of routes the application is mounted (active) for.
-  activeWhen: ["/"],
-  // If you uncomment this, run singleSpaNavigate('/settings') in the browser console
+  activeWhen: ["/settings"],
+  // If you uncomment this, run singleeSpaNavigate('/settings') in the browser console
   // to watch the app start out inactive and then mount.
   // activeWhen: ['/settings', '/dashboard']
 
   // Data to pass to the application
   customProps: {
     adjective: "first",
-    // You can pass in functions (or anything else) as well
-    // getAuthToken() {
-    //   return 'asdf7sf789sd7f9sd8f9sdfsdf9s'
-    // }
   },
 });
 
